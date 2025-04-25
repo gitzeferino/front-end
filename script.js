66427ff0c32d52f60a52ca1ee5142438e7e06589
gitzeferino <gitzeferino@gmail.com>
2025-04-25 19:29:20 -0300

    Alteração de Diretório
    Organizando o conteúdo

--- script.js
+++
@@ -1,9 +0,0 @@
-// Mensagem no container
-document.getElementById("mensagemContainer").innerText = "Bem-vindo! Esta é a div principal (container).";
-
-// Mensagens nas divs internas
-document.getElementById("div1").innerText = "Div 1: Conteúdo inserido via JavaScript.";
-document.getElementById("div2").innerText = "Div 2: Também com conteúdo dinâmico.";
-
-// Mensagem na div lateral
-document.getElementById("mensagemLateral").innerText = "Esta é a div lateral. Aqui também vai conteúdo!";
